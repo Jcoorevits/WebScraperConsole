634cf1c3ef8788b06ed9c3dfa9cce37231ff2b0e
Jeremy Coorevits <71607941+Jcoorevits@users.noreply.github.com>
2021-12-09 11:50:52 +0100

    first commit

---
+++ WebScraper/views/MainPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebScraper.views
+{
+    class MainPage
+    {
+        static void Main(string[] args)
+        {
+            Youtube.Scraper();
+        }
+    }
+}
---
+++ WebScraper/views/Youtube.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebScraper.views
+{
+    class Youtube
+    {
+        public static void Scraper()
+        {
+            // Configure Chrome options to use in chrome driver
+            ChromeOptions option = new ChromeOptions();
+            // option.AddArgument("--headless");
+            // option.AddArgument("--silent");
+            // option.AddArgument("--disable-gpu");
+            option.AddArgument("--log-level=3");
+
+
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.SuppressInitialDiagnosticInformation = true;
+
+            // Make new Chrome driver and go to Youtube
+            IWebDriver driver = new ChromeDriver(service, option);
+            driver.Navigate().GoToUrl("https://www.youtube.com/");
+
+
+            // Accept cookies
+            driver.FindElement(By.XPath(
+                    "/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[5]/div[2]/ytd-button-renderer[2]/a/tp-yt-paper-button"))
+                .Click();
+
+            // User input search term and submit
+            Console.Write("What would u like to search: ");
+            var input = Console.ReadLine();
+            var findElement = driver.FindElement(By.XPath(
+                "/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"));
+            findElement.Click();
+            findElement.SendKeys(input);
+            findElement.Submit();
+
+            // filter search term on new video's
+            var filters = driver.FindElement(By.XPath(
+                "/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/div/ytd-toggle-button-renderer/a/tp-yt-paper-button/yt-formatted-string"));
+
+            filters.Click();
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(By.XPath(
+                    "/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[1]/ytd-search-filter-renderer[2]/a/div/yt-formatted-string"))
+                .Click();
+            System.Threading.Thread.Sleep(1000);
+
+            filters.Click();
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(By.XPath(
+                    "/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[5]/ytd-search-filter-renderer[2]/a/div/yt-formatted-string"))
+                .Click();
+            System.Threading.Thread.Sleep(1000);
+
+            // Look for title, author, view
+            var songTitles = driver.FindElements(By.CssSelector("#video-title > yt-formatted-string"));
+            var songAuthor = driver.FindElements(By.CssSelector("#channel-info"));
+            var songViews = driver.FindElements(By.CssSelector("#metadata-line"));
+
+            // Get first 5 objects
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(songTitles[i].Text);
+                Console.WriteLine(songAuthor[i].Text);
+                Console.WriteLine(songViews[i].Text);
+            }
+        }
+    }
+}
