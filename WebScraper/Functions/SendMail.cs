ce8c84719ca3026cff95f9ba88d61a8974a1f5f7
Jeremy Coorevits <71607941+Jcoorevits@users.noreply.github.com>
2021-12-16 13:41:23 +0100

    quick fix

--- WebScraper/Functions/SendMail.cs
+++ WebScraper/Functions/SendMail.cs
@@ -9,32 +9,6 @@ namespace WebScraper.Functions
     {
         public static void Appointment(string appointment, string url)
         {
-            /*SmtpClient client = new SmtpClient()
-            {
-                Host = "smtp@gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential()
-                {
-                    UserName = "doctorscraper2000@gmail.com",
-                    Password = Password.Pass()
-                },
-                PickupDirectoryLocation = @"C:\CSharp\demoScraper"
-            };
-            MailAddress from = new MailAddress("doctorScraper2000@scraped.com", "Scraped Appointment");
-            MailAddress to = new MailAddress("r0843822@student.thomasmore.be");
-            MailMessage message = new MailMessage()
-            {
-                From = from,
-                Subject = "Appointment was found!",
-                Body = "An appointment has become available on: " + appointment + "\n" + url
-            };
-            message.To.Add(to);
-
-
-            client.Send(message);*/
             var fromAddress = new MailAddress("doctorscraper2000@gmail.com", "Web Scraper");
             var toAddress = new MailAddress("r0843822@student.thomasmore.be", "My Name");
             string fromPassword = Password.Pass();
